7483da3945b1da06f81efa517008117716bbaf44
kalidzo <kalidzodev@gmail.com>
2023-01-08 20:08:31 +0500

    Header.top Done

---
+++ src/pages/atms.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Atms() {
+  return (
+    <div>
+        <ul id='#sales'>
+           <li>
+                Bankomatlar
+            </li> 
+        </ul>
+    </div>
+
+  )
+}
